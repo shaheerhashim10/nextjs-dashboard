a6aa52b3b91b1813200646743d10d0fe65cb58ae
Shaheer Hashim <shaheerhashim10@gmail.com>
2024-06-11 16:29:13 +0500

    Add Error Handling

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -16,26 +16,32 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export const createInvoice = async (formData: FormData) => {
+export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
 
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-
+  try {
+    await sql`
+        INSERT INTO invoices (customer_id, amount, status, date)
+        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+      `;
+  } catch (error) {
+    return {
+      message: 'Database Error: Failed to Create Invoice.',
+    };
+  }
   // Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
-};
+}
 
-export const updateInvoice = async (id: string, formData: FormData) => {
+export async function updateInvoice(id: string, formData: FormData) {
   const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
@@ -44,17 +50,32 @@ export const updateInvoice = async (id: string, formData: FormData) => {
 
   const amountInCents = amount * 100;
 
-  await sql`
-    UPDATE invoices
-    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-    WHERE id = ${id}
-  `;
-
+  try {
+    await sql`
+          UPDATE invoices
+          SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+          WHERE id = ${id}
+        `;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Update Invoice.' };
+  }
+  /* 
+    Note how redirect is being called outside of the try/catch block. 
+    This is because redirect works by throwing an error, which would be caught by the catch block. 
+    To avoid this, you can call redirect after try/catch. 
+    redirect would only be reachable if try is successful.
+    */
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
-};
+}
 
-export const deleteInvoice = async (id: string) => {
-  await sql`DELETE FROM invoices WHERE id=${id}`;
-  revalidatePath('/dashboard/invoices');
-};
+export async function deleteInvoice(id: string) {
+  throw new Error('Failed to Delete Invoice');
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+    return { message: 'Deleted Invoice.' };
+  } catch (error) {
+    return { message: 'Database Error: Failed to Delete Invoice.' };
+  }
+}
