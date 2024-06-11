43663c1d77ad9b9eadcb5bd9c1bf7c02e5266170
Shaheer Hashim <shaheerhashim10@gmail.com>
2024-06-11 14:25:49 +0500

    Add Delete Invoice functionality

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -53,3 +53,8 @@ export const updateInvoice = async (id: string, formData: FormData) => {
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 };
+
+export const deleteInvoice = async (id: string) => {
+  await sql`DELETE FROM invoices WHERE id=${id}`;
+  revalidatePath('/dashboard/invoices');
+};
