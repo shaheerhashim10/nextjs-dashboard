f09951d7759f340e5c4c8c1d71aaf31ad1316fb4
Shaheer Hashim <shaheerhashim10@gmail.com>
2024-06-11 14:20:51 +0500

    Add Edit Invoice functionality with Server Actions

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,7 @@ const FormSchema = z.object({
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export const createInvoice = async (formData: FormData) => {
   const { customerId, amount, status } = CreateInvoice.parse({
@@ -30,6 +31,25 @@ export const createInvoice = async (formData: FormData) => {
   `;
 
   // Once the database has been updated, the /dashboard/invoices path will be revalidated, and fresh data will be fetched from the server.
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+};
+
+export const updateInvoice = async (id: string, formData: FormData) => {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 };
